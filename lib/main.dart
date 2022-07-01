1f00a6f1bb490a7e92528d703b0c6101cef68b2a
khairulanwary <hello.khairulanwarr@gmail.com>
2022-07-01 12:08:48 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Create BottomNavigationBar In Flutter',
+      home: ShowBottomNavigationBar(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class ShowBottomNavigationBar extends StatefulWidget {
+  const ShowBottomNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  State<ShowBottomNavigationBar> createState() =>
+      _ShowBottomNavigationBarState();
+}
+
+class _ShowBottomNavigationBarState extends State<ShowBottomNavigationBar> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Profile',
+      style: optionStyle,
+    ),
+    Text(
+      'Love',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: const Text('Create Bottom NavigationBar In Flutter',
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+        ),
+      ),
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Love',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.red,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
